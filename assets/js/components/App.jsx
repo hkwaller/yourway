8a06b98488378396bd49e4569857d60717cec94d
Hannes Waller <hkwaller@gmail.com>
2016-03-09 16:57:48 +0100

    adding stuff

--- assets/js/components/App.jsx
+++ assets/js/components/App.jsx
@@ -3,6 +3,7 @@ import Card from './Card.jsx';
 
 import turer from '../../lib/turer.json';
 import aktiviteter from '../../lib/aktiviteter.json';
+import overnatting from '../../lib/overnatting.json';
 
 
 class App extends Component {
@@ -11,8 +12,11 @@ class App extends Component {
 
     this.state = {
       selectedTours: [],
+      selectedActivities: [],
+      selectedOvernatting: [],
       turer: turer,
       aktiviteter: aktiviteter,
+      overnatting: overnatting,
       pris: 0
     }
   }
@@ -20,11 +24,20 @@ class App extends Component {
 
     this.state.turer.forEach((tur, index) => {
       if (tur.bilde === e.target.classList[1]) {
-        this.state.turer[index].selected = true;
-        this.setState({
-          selectedTours: this.state.selectedTours.concat([tur]),
-          pris: this.state.pris + tur.pris
-        });
+        if (this.state.turer[index].selected) {
+          this.state.turer[index].selected = false;
+          this.setState({
+            selectedTours: this.state.selectedTours.splice(1, 1),
+            pris: this.state.pris - tur.pris
+          });
+          return;
+        } else {
+          this.state.turer[index].selected = true;
+          this.setState({
+            selectedTours: this.state.selectedTours.concat([tur]),
+            pris: this.state.pris + tur.pris
+          });
+        }
       }
     })
 
@@ -33,11 +46,41 @@ class App extends Component {
   activityClick = (e) => {
     this.state.aktiviteter.forEach((aktivitet, index) => {
       if (aktivitet.bilde === e.target.classList[1]) {
-        this.state.aktiviteter[index].selected = true;
+        if (this.state.aktiviteter[index].selected) {
+          this.state.aktiviteter[index].selected = false;
+          this.setState({
+            selectedActivities: this.state.selectedActivities.splice(1, 1),
+            pris: this.state.pris - aktivitet.pris
+          });
+          return;
+        } else {
+          this.state.aktiviteter[index].selected = true;
+          this.setState({
+            selectedActivities: this.state.selectedActivities.concat([aktivitet]),
+            pris: this.state.pris + aktivitet.pris
+          });
+        }
+      }
+    })
+  }
 
-        this.setState({
-          pris: this.state.pris + aktivitet.pris
-        });
+  overnattingClick = (e) => {
+    this.state.overnatting.forEach((overnatting, index) => {
+      if (overnatting.bilde === e.target.classList[1]) {
+        if (this.state.overnatting[index].selected) {
+          this.state.overnatting[index].selected = false;
+          this.setState({
+            selectedOvernatting: this.state.selectedOvernatting.splice(1, 1),
+            pris: this.state.pris - overnatting.pris
+          });
+          return;
+        } else {
+          this.state.overnatting[index].selected = true;
+          this.setState({
+            selectedOvernatting: this.state.selectedOvernatting.concat([overnatting]),
+            pris: this.state.pris + overnatting.pris
+          });
+        }
       }
     })
   }
@@ -54,15 +97,48 @@ class App extends Component {
     })
   }
 
+  renderSleepOptions() {
+    return this.state.overnatting.map(overnatting => {
+      return <Card key={overnatting.navn} objekt={overnatting} clickHandler={this.overnattingClick} />
+    })
+  }
+
   render() {
     return (
-      <div>
-        <div className="card-row">
-          {::this.renderTours()}
+      <div className="app">
+        <div className="left">
+          <div className="row-heading">
+            <h2>Geografi</h2>
+          </div>
+          <div className="card-row">
+            {::this.renderTours()}
+          </div>
+          {this.state.selectedTours.length > 0 ? <div><div className="row-heading"><h2>Aktiviteter</h2></div><div className="card-row">{::this.renderActivities()}</div></div> : null}
+          {this.state.selectedActivities.length > 0 ? <div><div className="row-heading"><h2>Overnatting</h2></div><div className="card-row">{::this.renderSleepOptions()}</div></div> : null}
         </div>
-        {this.state.selectedTours.length > 0 ? <div className="card-row">{::this.renderActivities()}</div> : null}
-        <div className="price-row">
-          {this.state.pris}
+        <div className="right">
+          <ul className="hero">
+            <li><img src="img/green.png" />Krever ingen forkunnskaper</li>
+            <li><img src="img/orange.png" />Moderat</li>
+            <li><img src="img/red.png" />Krevende</li>
+          </ul>
+          <div className="price-row">
+            <div>{this.state.pris}</div>
+          </div>
+          <ul className="selected-things">
+            {this.state.selectedTours.map(tur => {
+              return <li>{tur.navn} - {tur.pris} kr</li>
+            })}
+
+            {this.state.selectedActivities.map(aktivitet => {
+              return <li>{aktivitet.navn} - {aktivitet.pris} kr</li>
+            })}
+
+            {this.state.selectedOvernatting.map(overnatting => {
+              return <li>{overnatting.navn} - {overnatting.pris} kr</li>
+            })}
+
+          </ul>
         </div>
       </div>
     )
--- assets/js/components/Card.jsx
+++ assets/js/components/Card.jsx
@@ -3,16 +3,18 @@ import classNames from 'classnames';
 
 const Card = ({objekt, clickHandler}) => {
 
-  const { navn, beskrivelse, pris, bilde, selected } = objekt;
+  const { navn, beskrivelse, pris, bilde, selected, vanskelighetsgrad } = objekt;
 
   const cardClassNames = classNames('card', bilde, { 'selected': selected });
+  const difficultyClassNames = classNames('card-difficulty', vanskelighetsgrad)
 
   return (
     <div className={cardClassNames} onClick={clickHandler}>
+      <div className={difficultyClassNames}></div>
       <div className="card-name">{navn}</div>
       <div className="card-details">
-        <div className="card-price">{pris} kr</div>
         <div className="card-description">{beskrivelse}</div>
+        <div className="card-price">{pris} kr</div>
       </div>
     </div>
   )
